2044a5250b9ddf48216b90ed46382f69978d19ed
leejunyoung228 <junyeongi748@gmail.com>
2021-06-23 16:45:17 +0900

    project

--- project/project.ino
+++ project/project.ino
@@ -2,6 +2,7 @@
 #define PIN 3
 #define LEDNUM 8
 int s = A1;
+int n=0;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDNUM, PIN, NEO_GRB + NEO_KHZ800);
 #define bu 4
 void setup() {
@@ -11,12 +12,24 @@ void setup() {
 int r,g,b;
 void loop() {
   Serial.println(analogRead(s));
-  if(analogRead(s)>1000){
+  if(analogRead(s)>=1023&&n==1){
     for(int i=0;i<8;i++){
-      //strip.setPixelColor(i, 0, 0, 0);
-      //strip.show();
-      }
-     }
+      strip.setPixelColor(i, 0, 0, 0);
+      strip.show();
+    }
+    n=0;
+  }
+  if(analogRead(s)>=1023&&n==0){
+    r=random(0,255);
+    g=random(0,255);
+    b=random(0,255);
+    for(int i=0;i<8;i++){
+      strip.setPixelColor(i, r, g, b);
+      strip.show();
+    }
+    n=1;
+  }
+  
   if(digitalRead(bu)==HIGH){
       r=random(0,255);
       g=random(0,255);
@@ -25,5 +38,7 @@ void loop() {
       strip.setPixelColor(i, r, g, b);
       strip.show();
     }
+    n=1;
+    delay(50);
   }
 }
